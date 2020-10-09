51fd1bdfc50c91d2ae85e891d881562706aa20e8
Jules Sayer <jules.sayer@protonmail.com>
2020-10-09 13:28:49 +0200

    Added User Login/Logout & routes

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -2,12 +2,18 @@
 
 namespace app\controllers;
 
+use app\helpers\Auth;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 final class AppController extends Controller {
     public function index(Request $request, Response $response, array $args): Response {
-        return $response->withJson(['response' => true]);
+        if(Auth::check()) {
+            $response = $response->withRedirect($this->router->pathFor('app.home'));
+        } else {
+            $response = $response->withRedirect($this->router->pathFor('app.login'));
+        }
+        return $response;
     }
 
 }
